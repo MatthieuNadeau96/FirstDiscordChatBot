5b5bb97c0c1322e86d12cc5d0b0833275ef574a0
Matthieu <matthieunadeau96@gmail.com>
2019-01-09 14:10:28 -0500

    Added new Egg imgs and renamed some of the old ones to match

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client();
 client.on('message', (msg) => {
 
   if (msg.content === '!giveMeEgg') {
-    const eggArray = ['./assets/imgs/egg.png', './assets/imgs/eggFace.png', './assets/imgs/dwayne.png'];
+    const eggArray = ['./assets/imgs/egg.png', './assets/imgs/eggFace.png', './assets/imgs/eggDwayne.png'];
     let img = eggArray[Math.floor(Math.random()*eggArray.length)];
     const buffer = fs.readFileSync(img);
     const attachment = new Attachment(buffer, img);
