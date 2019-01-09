eb7bf978358ac6be5a6e7bdb717a733df8804547
Matthieu <matthieunadeau96@gmail.com>
2019-01-09 13:03:32 -0500

    Added imgs and conditions to retrieve them

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const Discord = require('discord.js');
+const { Client, Attachment } = require('discord.js');
 
 const token = (process.env.SECRET_MESSAGE);
 
-const client = new Discord.Client();
+const fs = require('fs');
+
+const client = new Client();
 
 client.on('message', (msg) => {
   if (msg.content === '!egg') {
-    msg.channel.send();
+    const buffer = fs.readFileSync('./assets/imgs/egg.png');
+    const attachment = new Attachment(buffer, './assets/imgs/egg.png')
+    msg.channel.send(attachment);
+  }
+
+  if (msg.content === '!eggFace') {
+    const buffer = fs.readFileSync('./assets/imgs/eggFace.png');
+    const attachment = new Attachment(buffer, './assets/imgs/eggFace.png')
+    msg.channel.send(attachment);
+  }
+
+  if (msg.content === '!dwayne') {
+    const buffer = fs.readFileSync('./assets/imgs/dwayne.png');
+    const attachment = new Attachment(buffer, './assets/imgs/dwayne.png')
+    msg.channel.send(attachment);
   }
 
 });
